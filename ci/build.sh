91cfdcd70ca541f07dbd753815fcd00e830493df
Jon Mayo <jmayo@nvidia.com>
2021-03-21 15:43:06 -0700

    [ci] shellcheck build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -x
 set -e
-eval $(ssh-agent -s)
+eval "$(ssh-agent -s)"
 echo "${SSH_PRIVATE_KEY}" | ssh-add - &> /dev/null
-mkdir -p ${HOME}/.ssh
-chmod 700 ${HOME}/.ssh
-ssh-add -L > ${HOME}/.ssh/id_rsa.pub
+mkdir -p "${HOME}/.ssh"
+chmod 700 "${HOME}/.ssh"
+ssh-add -L > "${HOME}/.ssh/id_rsa.pub"
 cd ./ci
 terraform init -input=false
-export CI_COMMIT_TAG="$(git describe --abbrev=0 --tags)"
+CI_COMMIT_TAG="$(git describe --abbrev=0 --tags)"
+export CI_COMMIT_TAG
 export FORCE=true
 export REGISTRY=${IMAGE}
 export DRIVER_VERSION=${VERSION}
