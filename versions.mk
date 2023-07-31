1836f618a987b5362ae0dcaa7e386976b1ec8a08
Christopher Desiniotis <cdesiniotis@nvidia.com>
2023-07-31 11:35:45 -0700

    Bump R535 to latest version
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 # DRIVER_VERSIONS contains latest version in all active datacenter branches
-DRIVER_VERSIONS ?= 450.248.02 470.199.02 525.125.06 535.54.03
+DRIVER_VERSIONS ?= 450.248.02 470.199.02 525.125.06 535.86.10
 
 CUDA_VERSION := 12.2.0
