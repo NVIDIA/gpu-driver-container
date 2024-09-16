8c35f6749f4a7525be727b6759e4540df2240964
Tariq Ibrahim <tibrahim@nvidia.com>
2024-09-16 10:29:25 -0700

    ubuntu24.04 precompile image support
    Signed-off-by: shiva kumar <shivaku@nvidia.com>

--- tests/scripts/ci-precompiled-helpers.sh
+++ tests/scripts/ci-precompiled-helpers.sh
@@ -1,6 +1,6 @@
 get_kernel_versions_to_test() {
     if [[ "$#" -ne 4 ]]; then
-	    echo " Error:$0 must be called with BASE_TARGET DRIVER_BRANCHES DRIVER_BRANCHES DIST" >&2
+	    echo " Error:$0 must be called with BASE_TARGET KERNEL_FLAVORS DRIVER_BRANCHES DIST" >&2
 	    exit 1
     fi
 
@@ -11,10 +11,6 @@ get_kernel_versions_to_test() {
 
     kernel_versions=()
     for kernel_flavor in "${KERNEL_FLAVORS[@]}"; do
-        # FIXME -- remove if condition, once azure kernel upgrade starts working
-        if [[ "$kernel_flavor" == "azure" ]]; then
-            continue
-        fi
         for DRIVER_BRANCH in "${DRIVER_BRANCHES[@]}"; do
             source ./tests/scripts/findkernelversion.sh "$BASE_TARGET" "${kernel_flavor}" "$DRIVER_BRANCH" "$DIST" >&2
             if [[ "$should_continue" == true ]]; then
@@ -25,6 +21,11 @@ get_kernel_versions_to_test() {
             KERNEL_VERSION=$(echo "$KERNEL_VERSION" | tr -d ' \n')
             kernel_versions+=("$KERNEL_VERSION")
         fi
+    done
+    # Remove duplicates
+    kernel_versions=($(printf "%s\n" "${kernel_versions[@]}" | sort -u))
+    for i in "${!kernel_versions[@]}"; do
+        kernel_versions[$i]="${kernel_versions[$i]}-$DIST"
     done
     echo "${kernel_versions[@]}"
 }
