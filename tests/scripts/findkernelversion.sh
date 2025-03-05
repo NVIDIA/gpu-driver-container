a72a40d3b6db8ad1cd5d59c3a8615e7ee5c81a01
shiva kumar <shivaku@nvidia.com>
2025-03-05 10:15:40 +0530

    Atrifacts mismatch kernel version fix
    Signed-off-by: shiva kumar <shivaku@nvidia.com>

--- tests/scripts/findkernelversion.sh
+++ tests/scripts/findkernelversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $# -ne 4 ]]; then
-	echo " BASE_TARGET KERNEL_FLAVOR DRIVER_BRANCH DIST are required"
+if [[ $# -ne 5 ]]; then
+	echo " BASE_TARGET KERNEL_FLAVOR DRIVER_BRANCH DIST LTS_KERNEL are required"
 	exit 1
 fi
 
@@ -9,6 +9,7 @@ export BASE_TARGET="${1}"
 export KERNEL_FLAVOR="${2}"
 export DRIVER_BRANCH="${3}"
 export DIST="${4}"
+export LTS_KERNEL="${5}"
 
 export REGCTL_VERSION=v0.7.1
 mkdir -p bin
@@ -17,30 +18,20 @@ chmod a+x bin/regctl
 export PATH=$(pwd)/bin:${PATH}
 
 # calculate kernel version of latest image
-regctl image get-file ghcr.io/nvidia/driver:base-${BASE_TARGET}-${KERNEL_FLAVOR}-${DRIVER_BRANCH} /var/kernel_version.txt ./kernel_version.txt 2>/dev/null || true
-if [[ -f ./kernel_version.txt && -s ./kernel_version.txt ]]; then
-    # File exists and is not empty
-    export $(grep -oP 'KERNEL_VERSION=[^ ]+' ./kernel_version.txt)
-    rm -f kernel_version.txt
-else
-    # Define variables for artifact pattern
-    prefix="kernel-version-${DRIVER_BRANCH}-${LTS_KERNEL}"
-    suffix="${kernel_flavor}-${DIST}"
-    artifacts=$(gh api -X GET /repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts --jq '.artifacts[].name')
-    # Use a loop or a pattern to find the matching artifact dynamically
-    for artifact in $artifacts; do
-        # TODO remove this check once nvidia avaialble
-        # currently for ubuntu24.04 kernel_flavor = nvidia-lowlatency
-        if [[ $artifact == $prefix*-$suffix ]]; then
-            gh run download --name "$artifact" --dir ./
-            tar -xf $artifact.tar 
-            rm -f $artifact.tar
-            export $(grep -oP 'KERNEL_VERSION=[^ ]+' ./kernel_version.txt)
-            rm -f kernel_version.txt
-            break
-        fi
-    done
-fi
+prefix="kernel-version-${DRIVER_BRANCH}-${LTS_KERNEL}"
+suffix="${kernel_flavor}-${DIST}"
+artifacts=$(gh api -X GET /repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts --paginate --jq '.artifacts[].name')
+# find the matching artifact dynamically
+for artifact in $artifacts; do
+    if [[ $artifact == $prefix*-$suffix ]]; then
+        gh run download --name "$artifact" --dir ./
+        tar -xf $artifact.tar 
+        rm -f $artifact.tar
+        export $(grep -oP 'KERNEL_VERSION=[^ ]+' ./kernel_version.txt)
+        rm -f kernel_version.txt
+        break
+    fi
+done
 
 # calculate driver tag
 status=0
@@ -50,3 +41,4 @@ if [[ $status -eq 0 ]]; then
 else
     export should_continue=true
 fi
+export should_continue=true
