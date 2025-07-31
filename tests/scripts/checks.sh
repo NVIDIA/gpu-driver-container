12089e00d4dce4038f1c52d997e27cd96c97da11
Shiva Kumar (SW-CLOUD) <shivaku@nvidia.com>
2025-07-31 20:11:02 +0530

    Test Not for review holodeck kernel version change

--- tests/scripts/checks.sh
+++ tests/scripts/checks.sh
@@ -7,7 +7,6 @@ check_pod_ready() {
 	echo "Checking $pod_label pod"
 	
 	kubectl get pods -lapp=$pod_label -n ${TEST_NAMESPACE}
-
 	echo "Checking $pod_label pod readiness"
 
 	if kubectl wait -n ${TEST_NAMESPACE} --for=condition=Ready pod -l app=$pod_label --timeout ${pod_status_time_out}; then
