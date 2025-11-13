add9ce072af7e40cfd0d4b17f56615b611c98427
Arjun <agadiyar@nvidia.com>
2025-11-13 22:01:55 +0000

    Bump 580.105.08
    Signed-off-by: Arjun <agadiyar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 # DRIVER_VERSIONS contains latest version in all active datacenter branches
-DRIVER_VERSIONS ?= 535.274.02 570.195.03 580.95.05
+DRIVER_VERSIONS ?= 535.274.02 570.195.03 580.105.08
 
 GOLANG_VERSION := 1.25.2
