8229eb336b138c554229def2541eafd53a810933
Mathieu Parent <math.parent@etik.com>
2026-01-02 22:57:36 +0100

    Add Debian 13
    See https://github.com/NVIDIA/gpu-driver-container/issues/530
    
    Signed-off-by: Mathieu Parent <math.parent@etik.com>

---
+++ debian13/install.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+set -eu
+
+download_installer () {
+    DRIVER_ARCH=${TARGETARCH/amd64/x86_64} && DRIVER_ARCH=${DRIVER_ARCH/arm64/aarch64} && curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-$DRIVER_ARCH-$DRIVER_VERSION.run && \
+    chmod +x  NVIDIA-Linux-$DRIVER_ARCH-$DRIVER_VERSION.run;
+}
+
+dep_install () {
+    if [ "$TARGETARCH" = "amd64" ]; then
+        dpkg --add-architecture i386 && \
+            apt-get update && apt-get install -y --no-install-recommends \
+            apt-utils \
+            build-essential \
+            ca-certificates \
+            curl \
+            kmod \
+            file \
+            gnupg \
+            libelf-dev \
+            libglvnd-dev \
+            pkg-config && \
+        rm -rf /var/lib/apt/lists/*
+    elif [ "$TARGETARCH" = "arm64" ]; then
+        dpkg --add-architecture arm64 && \
+            apt-get update && apt-get install -y \
+            build-essential \
+            ca-certificates \
+            curl \
+            kmod \
+            file \
+            gnupg \
+            libelf-dev \
+            libglvnd-dev && \
+        rm -rf /var/lib/apt/lists/*
+    fi
+}
+
+setup_cuda_repo() {
+    # Fetch public CUDA GPG key and configure apt to only use this key when downloading CUDA packages
+    OS_ARCH=${TARGETARCH/amd64/x86_64} && OS_ARCH=${OS_ARCH/arm64/sbsa};
+    curl -fSsL "https://developer.download.nvidia.com/compute/cuda/repos/debian13/${OS_ARCH}/cuda-keyring_1.1-1_all.deb" -o cuda-keyring_1.1-1_all.deb
+    dpkg -i cuda-keyring_1.1-1_all.deb
+}
+
+fabricmanager_install() {
+  if [ "$DRIVER_BRANCH" -ge "580" ]; then
+    apt-get install -y --no-install-recommends nvidia-fabricmanager=${DRIVER_VERSION}-1
+  else
+    apt-get install -y --no-install-recommends nvidia-fabricmanager-${DRIVER_BRANCH}=${DRIVER_VERSION}-1
+  fi
+}
+
+nscq_install() {
+  if [ "$DRIVER_BRANCH" -ge "580" ]; then
+    apt-get install -y --no-install-recommends libnvidia-nscq=${DRIVER_VERSION}-1
+  else
+    apt-get install -y --no-install-recommends libnvidia-nscq-${DRIVER_BRANCH}=${DRIVER_VERSION}-1
+  fi
+}
+
+# libnvsdm packages are not available for arm64
+nvsdm_install() {
+  if [ "$TARGETARCH" = "amd64" ]; then
+    if [ "$DRIVER_BRANCH" -ge "580" ]; then
+       apt-get install -y --no-install-recommends libnvsdm=${DRIVER_VERSION}-1
+    elif [ "$DRIVER_BRANCH" -ge "570" ]; then
+       apt-get install -y --no-install-recommends libnvsdm-${DRIVER_BRANCH}=${DRIVER_VERSION}-1
+    fi
+  fi
+}
+
+nvlink5_pkgs_install() {
+  if [ "$DRIVER_BRANCH" -ge "550" ]; then
+    apt-get install -y --no-install-recommends nvlsm infiniband-diags
+  fi
+}
+
+imex_install() {
+  if [ "$DRIVER_BRANCH" -ge "580" ]; then
+    apt-get install -y --no-install-recommends nvidia-imex=${DRIVER_VERSION}-1
+  elif [ "$DRIVER_BRANCH" -ge "550" ]; then
+    apt-get install -y --no-install-recommends nvidia-imex-${DRIVER_BRANCH}=${DRIVER_VERSION}-1;
+  fi
+}
+
+extra_pkgs_install() {
+  if [ "$DRIVER_TYPE" != "vgpu" ]; then
+      apt-get update
+
+      fabricmanager_install
+      nscq_install
+
+      echo "extra_pkgs_install $TARGETARCH"
+      if [ "$TARGETARCH" = "amd64" ]; then
+        echo "arm shouldn't be entering"
+        nvsdm_install
+      fi
+
+      nvlink5_pkgs_install
+      imex_install
+
+      rm -rf /var/lib/apt/lists/*
+  fi
+}
+
+if [ "$1" = "depinstall" ]; then
+  dep_install
+elif [ "$1" = "download_installer" ]; then
+  download_installer
+elif [ "$1" = "extra_pkgs_install" ]; then
+  extra_pkgs_install
+elif [ "$1" = "setup_cuda_repo" ]; then
+  setup_cuda_repo
+else
+  echo "Unknown function: $1"
+  exit 1
+fi
