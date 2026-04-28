75a365bcc8b4b45e485c93705a78d6aa75613cdf
Rahul Sharma <rahulsharm@nvidia.com>
2026-04-27 19:47:24 -0700

    build containerized drivers for R535TRD18, R580TRD8 and R595TRD2
    Signed-off-by: Rahul Sharma <rahulsharm@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 # DRIVER_VERSIONS contains latest version in all active datacenter branches
-DRIVER_VERSIONS ?= 535.288.01 580.126.20 595.58.03
+DRIVER_VERSIONS ?= 535.309.01 580.159.03 595.71.05
 
-GOLANG_VERSION := 1.26.1
+GOLANG_VERSION := 1.26.2
